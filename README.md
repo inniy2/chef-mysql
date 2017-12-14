5c21ed45c880828ee3d4470e99b3eb79e35287c9
Bae, SangSun | Sunny | OPS <sangsun.bae@rakuten.com>
2017-12-14 15:59:25 +0900

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-mysql
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-mysql
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
